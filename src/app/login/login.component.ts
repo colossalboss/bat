e18be70e271e75a64314c72e59333f81be326255
Bibi <ogbaragodwin@Ogbaras-MacBook-Pro-125963.local>
2020-02-02 00:53:51 +0100

    Try fix Google sign in

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -27,4 +27,14 @@ export class LoginComponent implements OnInit {
             '';
   }
 
+  onSignIn(googleUser) {
+    // tslint:disable-next-line:prefer-const
+    console.log('Hello');
+    let profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+
 }
